7d8d3136a6f5a35ccb1555f19028f34d524695c6
Compu22 <Compu22@Compu22>
2026-03-24 15:37:01 -0600

    Listo22

---
+++ src/principalpc22/PrincipalPC22.java
@@ -0,0 +1,17 @@
+package principalpc22;
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+public class PrincipalPC22 {
+
+    public static void main(String[] args) {
+    Scanner i = new Scanner (System.in);    
+    
+    int N1 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el 1° numero: "));
+    int N2 = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el 2° numero: "));
+    
+    JOptionPane.showMessageDialog(null,"Resultado: "+(N1+N2)+".");     
+    }
+    
+}
